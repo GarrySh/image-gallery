2cc3c84666d0e8a96c30dfc5d432defa2171a73f
GarrySh <sooo.lame@gmail.com>
2019-01-17 20:35:54 +1000

    Update 2019-01-17T10:35:33.009Z

--- gulp/tasks/build.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-
-import clean from './clean';
-import html from './html';
-import css from './css';
-import scripts from './scripts';
-
-export default gulp.series(clean, gulp.parallel(html, css, scripts));
